21efeff88af1483d3cb098bf6d74140a6b5120aa
Samuel Odekunle <Samuel@lgger.com>
2025-09-05 08:16:32 +0100

    chx005a

--- ingest-worker/.wrangler/tmp/bundle-yNqu2F/middleware-loader.entry.ts
+++ ingest-worker/.wrangler/tmp/bundle-yNqu2F/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-M8Hhu6\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-yNqu2F\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-M8Hhu6\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-yNqu2F\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-M8Hhu6\\middleware-insertion-facade.js";
+export * from "C:\\Users\\SamuelOdekunle.AzureAD\\DCOM360\\DCOM360\\ingest-worker\\.wrangler\\tmp\\bundle-yNqu2F\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
