3385aff1a76e773a84013cd6317913c8d2444ba5
Lordpixma <samuel@lgger.com>
2025-09-06 10:20:47 +0100

    ci: run D1 migrations in production to populate full countries list

--- ingest-worker/.wrangler/tmp/dev-kTHJn8/index.js
+++ ingest-worker/.wrangler/tmp/dev-kTHJn8/index.js
@@ -1806,10 +1806,10 @@ var require_fxp = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-5amZjx/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-mb2pFg/middleware-loader.entry.ts
 init_modules_watch_stub();
 
-// .wrangler/tmp/bundle-5amZjx/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-mb2pFg/middleware-insertion-facade.js
 init_modules_watch_stub();
 
 // src/index.ts
@@ -6261,7 +6261,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-5amZjx/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-mb2pFg/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -6294,7 +6294,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-5amZjx/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-mb2pFg/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
